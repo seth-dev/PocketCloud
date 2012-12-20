ff6ec28cc88e74492fd14ae01ab1a31df048fed5
Seth Ceriano <seth.i@me.com>
2012-12-19 18:08:15 -0800

    Initial Foundation commit.

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="default" />
+<meta name="viewport" content="user-scalable=no, width=device-width" />
+</head>
+<body>
+
+
+
+</body>
+</html>
