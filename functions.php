a9538fca46a8853bc800edb835cefcc54a880f7a
Seth Ceriano <seth.i@me.com>
2012-12-20 11:55:10 +0700

    Initial function commit

--- functions.php
+++ functions.php
@@ -1,5 +1,26 @@
 <?php
+/* functions.php for PocketCloud */
 
 
+/* Date and Time */
+
+$tz = date_default_timezone_get();
+static $date;
+$date = date('m/d/Y h:i:s a', time()); // Date and time
+
+/* System Variables (auto) */
+
+$requesturl="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+$clientip=$_SERVER['REMOTE_ADDR'];
+$useragent=$_SERVER['HTTP_USER_AGENT'];
+$isiPad = (bool) strpos($_SERVER['HTTP_USER_AGENT'],'iPad');
+
+/* Functions */
+
+function Init()
+{
+// Initialization Function
+
+}
 
 ?>
