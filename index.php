b1797c166f33770807bb750795cabb91cabfa396
Seth Ceriano <seth.i@me.com>
2012-12-20 11:58:08 +0700

    PHP Functions Moved
    PHP Functions moved to external file, and a little tidy-up of the code.

--- index.php
+++ index.php
@@ -1,30 +1,9 @@
 <?php
+/* Import Files */
+include 'functions.php';
+/* Coding goes below this line */
 
-/* Constant Variables */
-
-$version="0.1"; // Define version for reference
-$isbeta="1"; // Define if the version is beta. For info only.
-
-/* Date and Time */
-
-$tz = date_default_timezone_get();
-$date = date('m/d/Y h:i:s a', time()); // Date and time
-
-/* System Variables (auto) */
-
-$requesturl="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-$clientip=$_SERVER['REMOTE_ADDR'];
-$useragent=$_SERVER['HTTP_USER_AGENT'];
-
-/* Functions */
-
-function Init()
-{
-// Initialization Function
-
-}
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
