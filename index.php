9e70d72437d142dfe6ba71a781d62bc50a6c9276
Seth Ceriano <seth.i@me.com>
2012-12-20 11:25:11 +0700

    A Little Housekeeping
    Eliminated unused spaces, tidying script.

--- index.php
+++ index.php
@@ -23,7 +23,6 @@ function Init()
 // Initialization Function
 
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -32,26 +31,18 @@ function Init()
 <meta name="apple-mobile-web-app-capable" content="yes" />
 <meta name="apple-mobile-web-app-status-bar-style" content="default" />
 <meta name="viewport" content="user-scalable=no, width=device-width" />
-
 <!-- some scripts, shall we? -->
-
 <script src="jQuery.js"></script>
-
 <!-- Prevent Elastic Scrolling -->
-
 <script>
  function ScreenScroll(event) {
   // Window moving is a no-no
   event.preventDefault() ;
  }
 </script>
-
 <!-- end elastic prvention script -->
-
 </head>
 <body ontouchmove="ScreenScroll(event);" >
-
-
 
 </body>
 </html>
